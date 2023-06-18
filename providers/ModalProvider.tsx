cc6a5f3e4782aaefe20d81d433636f32a61c83ad
StarkLSH <seungheon1029@gmail.com>
2023-06-18 23:14:50 +0900

    feat: AuthModal UI 및 hook 추가

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-import Modal from "@/components/Modal";
+import AuthModal from "@/components/AuthModal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -17,14 +17,7 @@ const ModalProvider = () => {
 
   return (
     <>
-      <Modal
-        title="Test Modal"
-        description="description"
-        isOpen
-        onChange={() => {}}
-      >
-        Test Chidren
-      </Modal>
+      <AuthModal />
     </>
   );
 };
