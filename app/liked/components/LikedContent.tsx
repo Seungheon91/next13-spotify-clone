f277effecc07cf37020d5ca576ecb03197a6f211
StarkLSH <seungheon1029@gmail.com>
2023-06-27 23:02:13 +0900

    feat: 음악 재생 관련 컴포넌트 및 로직 작성

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -23,15 +23,7 @@ const LikedContent: React.FC<LikedContentProps> = ({ songs }) => {
 
   if (songs.length === 0) {
     return (
-      <div
-        className="
-        flex
-        flex-col
-        gap-y-2
-        w-full
-        text-neutral-400
-      "
-      >
+      <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
         No liked songs.
       </div>
     );
