35b7be09176a882054631502713cc62c568d9b09
StarkLSH <seungheon1029@gmail.com>
2023-07-27 23:48:25 +0900

    fix: 오타 수정

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server";
 import Stripe from "stripe";
+import { NextResponse } from "next/server";
 import { headers } from "next/headers";
 
 import { stripe } from "@/libs/stripe";
@@ -24,16 +24,16 @@ export async function POST(request: Request) {
   const body = await request.text();
   const sig = headers().get("Stripe-Signature");
 
-  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  const webhookSecret =
+    process.env.STRIPE_WEBHOOK_SECRET_LIVE ?? process.env.STRIPE_WEBHOOK_SECRET;
   let event: Stripe.Event;
 
   try {
     if (!sig || !webhookSecret) return;
-
     event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
-  } catch (error: any) {
-    console.log("Error message:" + error.message);
-    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
+  } catch (err: any) {
+    console.log(`❌ Error message: ${err.message}`);
+    return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
   }
 
   if (relevantEvents.has(event.type)) {
@@ -57,7 +57,7 @@ export async function POST(request: Request) {
             event.type === "customer.subscription.created"
           );
           break;
-        case "checkout.sesstion.completed":
+        case "checkout.session.completed":
           const checkoutSession = event.data.object as Stripe.Checkout.Session;
           if (checkoutSession.mode === "subscription") {
             const subscriptionId = checkoutSession.subscription;
@@ -73,7 +73,10 @@ export async function POST(request: Request) {
       }
     } catch (error) {
       console.log(error);
-      return new NextResponse("Webhook error", { status: 400 });
+      return new NextResponse(
+        'Webhook error: "Webhook handler failed. View logs."',
+        { status: 400 }
+      );
     }
   }
 
